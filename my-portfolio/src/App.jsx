126a4ad430b3173152b6cd8640c35b503ea3c7aa
Hiruni Ramanayaka <hiruniramanayaka9@gmail.com>
2025-06-17 18:32:02 +0530

    Create Education Component

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -6,7 +6,6 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Certificates from './components/Certificates';
-import Education from './components/Education';
 import Blogs from './components/Blogs';
 import Contact from './components/Contact';
 import Achievements from './components/Achievements';
@@ -22,7 +21,6 @@ function App() {
       <Skills />
       <Projects />
       <Certificates />
-      <Education />
       <Blogs />
       <Contact />
       <Achievements />
--- my-portfolio/src/components/Education.jsx
+++ my-portfolio/src/components/Education.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
-import {Container} from '@mui/material'
+import {Container, Typography, Box} from '@mui/material'
 
 const Education = () => {
   return (
     <>
         <Container id="education">
-            <div>Education</div>
+          <Box sx={{ display: 'flex', gap: 8 }}>
+            <Box sx={{ textAlign: 'left', flex: 1, borderLeft: 1, borderColor: 'grey.300', pl: 2 }}>
+              <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2, lineHeight: 1.6, color: "primary.light"}}>Bachelor of Science (Hons) in Software Engineering</Typography>
+              <Typography variant="body1" sx={{lineHeight: 1.6}}>University of Kelaniya</Typography>
+              <Typography variant="body2" sx={{lineHeight: 1.6}}>2023 – Present</Typography>
+            </Box>
+            <Box sx={{ textAlign: 'left', flex: 1, borderLeft: 1, borderColor: 'grey.300', pl: 2}}>
+              <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2, lineHeight: 1.6, color: "primary.light"}}>GCE A/L (Advanced Level)</Typography>
+              <Typography variant="body1" sx={{lineHeight: 1.6}}>Bandaranayake Central Collage Veyangoda</Typography>
+              <Typography variant="body2" sx={{lineHeight: 1.6}}>Physical Stream</Typography>
+              <Typography variant="body2" sx={{lineHeight: 1.6}}>2021</Typography>
+              <Typography variant="body2" sx={{ fontStyle: 'italic', lineHeight: 1.8 }}>1A 2B's</Typography>
+            </Box>
+            <Box sx={{ textAlign: 'left', flex: 1, borderLeft: 1, borderColor: 'grey.300', pl: 2}}>
+              <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2, lineHeight: 1.6, color: "primary.light"}}>GCE O/L (Ordinary Level)</Typography>
+              <Typography variant="body1" sx={{lineHeight: 1.6}}>Bandaranayake Central Collage Veyangoda</Typography>
+              <Typography variant="body2" sx={{lineHeight: 1.6}}>2017</Typography>
+              <Typography variant="body2" sx={{ fontStyle: 'italic', lineHeight: 1.6 }}>9A's</Typography>
+            </Box>
+          </Box>
         </Container>
     </>
   )
